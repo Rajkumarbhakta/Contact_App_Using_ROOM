768f1aae9498911dd031bd008162c4d587bcdf6f
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2023-02-07 13:13:00 +0530

    contact view

---
+++ app/src/main/java/com/rkbapps/contactappusingroom/ContactViewActivity.java
@@ -0,0 +1,128 @@
+package com.rkbapps.contactappusingroom;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.room.Room;
+
+import com.rkbapps.contactappusingroom.db.Contact;
+import com.rkbapps.contactappusingroom.db.ContactDatabase;
+
+public class ContactViewActivity extends AppCompatActivity {
+
+    ContactDatabase cd;
+
+    TextView contactAvtar;
+    ImageButton btnCall, btnMassage, btnVideoCall;
+    Button btnEditContact;
+    TextView txtName, txtEmail, txtPhoneNumber;
+    String strName;
+
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_view);
+        long id = getIntent().getLongExtra("id", 0);
+
+        contactAvtar = findViewById(R.id.contactAvtar);
+        btnCall = findViewById(R.id.btnCall);
+        btnMassage = findViewById(R.id.btnMassage);
+        btnVideoCall = findViewById(R.id.btnVideoCall);
+        btnEditContact = findViewById(R.id.btnEditContact);
+        txtName = findViewById(R.id.textName);
+        txtPhoneNumber = findViewById(R.id.txtPhoneNumber);
+        txtEmail = findViewById(R.id.txtMailAdress);
+
+        Toolbar toolbar = findViewById(R.id.toolBarContactView);
+        setSupportActionBar(toolbar);
+
+        cd = Room.databaseBuilder(this, ContactDatabase.class, "ContactDB").allowMainThreadQueries().build();
+
+        Contact contact = cd.getContactDao().getContact(id);
+
+        setData(contact);
+
+        btnEditContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog d = new Dialog(ContactViewActivity.this);
+                d.setContentView(R.layout.add_contact_dialog);
+                Button cancel = d.findViewById(R.id.btnCancel);
+                Button save = d.findViewById(R.id.btnSave);
+                EditText name = d.findViewById(R.id.etName);
+                EditText email = d.findViewById(R.id.etEmail);
+                EditText mobileNumber = d.findViewById(R.id.etMobileNumber);
+                name.setText(strName);
+                email.setText(contact.getEmailId());
+                mobileNumber.setText(contact.getMobileNumber());
+                save.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        contact.setName(name.getText().toString().trim());
+                        contact.setMobileNumber(mobileNumber.getText().toString().trim());
+                        contact.setEmailId(email.getText().toString().trim());
+
+                        cd.getContactDao().updateContact(contact);
+                        setData(contact);
+                        d.dismiss();
+
+                    }
+                });
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        d.dismiss();
+                    }
+                });
+                d.show();
+            }
+        });
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:+91"+contact.getMobileNumber()));
+                startActivity(Intent.createChooser(i, "Call"));
+            }
+        });
+
+        btnMassage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                i.setData(Uri.parse("sms:+91"+contact.getMobileNumber()));
+                startActivity(Intent.createChooser(i,"Sent massage"));
+            }
+        });
+
+        btnVideoCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:+91"+contact.getMobileNumber()));
+                startActivity(Intent.createChooser(i, "Call"));
+            }
+        });
+
+    }
+
+    private void setData(Contact contact){
+        strName = contact.getName();
+        char c[] = strName.toCharArray();
+
+        contactAvtar.setText("" + c[0]);
+        txtName.setText(strName);
+        txtPhoneNumber.setText(contact.getMobileNumber());
+        txtEmail.setText(contact.getEmailId());
+    }
+}
--- app/src/main/java/com/rkbapps/contactappusingroom/MainActivity.java
+++ app/src/main/java/com/rkbapps/contactappusingroom/MainActivity.java
@@ -2,16 +2,20 @@ package com.rkbapps.contactappusingroom;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.rkbapps.contactappusingroom.db.Contact;
 import com.rkbapps.contactappusingroom.db.ContactDatabase;
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     FloatingActionButton btnAdd;
+    Toolbar toolbar;
     ContactDatabase contactDatabase;
     List<Contact> contactList = new ArrayList<>();
     MyAdapter adapter;
@@ -33,25 +38,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recyclerView);
+        toolbar=findViewById(R.id.toolBar);
         btnAdd = findViewById(R.id.addBtn);
+        setSupportActionBar(toolbar);
+
+
         contactDatabase = Room.databaseBuilder(getApplicationContext(), ContactDatabase.class, "ContactDB")
                 .allowMainThreadQueries()
                 .build();
-
-        contactList.addAll(contactDatabase.getContactDao().getAllContacts());
-        adapter = new MyAdapter(this, contactList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+        setData();
 
         recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
+                long currentId = contactList.get(position).getId();
 
+                Intent i = new Intent(MainActivity.this,ContactViewActivity.class);
+                i.putExtra("id",currentId);
+                startActivity(i);
             }
 
             @Override
             public void onLongClick(View view, int position) {
-
+                new MaterialAlertDialogBuilder(MainActivity.this).setTitle("Alert!")
+                        .setMessage("Are you sure to delete this contact?").setPositiveButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Do nothing
+                            }
+                        }).setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //delete a contact
+                                contactDatabase.getContactDao().deleteContact(contactList.get(position));
+                                contactList.remove(position);
+                                adapter.notifyDataSetChanged();
+                            }
+                        }).setIcon(R.drawable.clear)
+                        .show();
             }
         }));
 
@@ -83,8 +107,12 @@ public class MainActivity extends AppCompatActivity {
                 d.show();
             }
         });
+    }
 
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setData();
     }
 
     private void createContact(String name, String mobileNumber, String email) {
@@ -113,5 +141,13 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    private void setData(){
+        contactList.clear();
+        contactList.addAll(contactDatabase.getContactDao().getAllContacts());
+        adapter = new MyAdapter(this, contactList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
